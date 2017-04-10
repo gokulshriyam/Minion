0f32c7e534070a45b4c35c60bab8fc57e3fc0987
Gokul Shrinivas <gokul.shriyam@gmail.com>
2017-04-10 09:43:42 +0530

    Add files via upload

---
+++ NILMTK/nilmtk/dataset_converters/combed/convert_combed.py
@@ -0,0 +1,98 @@
+from __future__ import print_function, division
+from os.path import join, isdir, dirname, abspath
+from os import getcwd
+import os
+from sys import getfilesystemencoding
+from inspect import currentframe, getfile, getsourcefile
+from collections import OrderedDict
+from six import iteritems
+
+import pandas as pd
+from nilm_metadata import convert_yaml_to_hdf5
+
+from nilmtk.datastore import Key
+from nilmtk.measurement import LEVEL_NAMES
+from nilmtk.utils import check_directory_exists, get_datastore
+
+#{"load_type": {"floor/wing":meter_number_in_nilmtk}
+acad_block_meter_mapping = {'Building Total Mains': {'0': 1},
+                            'Lifts': {'0': 2},
+                            'Floor Total': {'1': 3, '2': 4, '3': 5, '4': 6, '5': 7},
+                            'AHU': {'0': 8, '1': 9, '2': 10, '5': 11},
+                            'Light': {'3': 12},
+                            'Power Sockets': {'3': 13},
+                            'UPS Sockets': {'3': 14}}
+
+lecture_block_meter_mapping = {'Building Total Mains': {'0': 1},
+                               'Floor Total': {'0': 2, '1': 3, '2': 4},
+                               'AHU': {'1': 5, '2': 6, '3': 7}}
+
+overall_dataset_mapping = OrderedDict({'Academic Block': acad_block_meter_mapping,
+                                       'Lecture Block': lecture_block_meter_mapping})
+
+building_number_mapping = {'Academic Block': 1, 'Lecture Block': 2}
+
+
+column_mapping = OrderedDict({
+    'Power': ('power', 'active'),
+    'Energy': ('energy', 'active'),
+    'Current': ('current', '')})
+
+
+def convert_combed(combed_path, output_filename, format='HDF'):
+    """
+    Parameters
+    ----------
+    combed_path : str
+        The root path of the combed dataset.
+    output_filename : str
+        The destination HDF5 filename (including path and suffix).
+    """
+
+    check_directory_exists(combed_path)
+
+    # Open store
+    store = get_datastore(output_filename, format, mode='w')
+
+    for building_name, building_mapping in iteritems(overall_dataset_mapping):
+        for load_name, load_mapping in iteritems(building_mapping):
+            for load_mapping_path, meter_number in iteritems(load_mapping):
+                building_number = building_number_mapping[building_name]
+                key = Key(building=building_number, meter=meter_number)
+                dfs = []
+                for attribute in column_mapping.keys():
+                    filename_attribute = join(combed_path, building_name, load_name, load_mapping_path, "%s.csv" %attribute)
+                    if os.path.isfile(filename_attribute):
+                        exists = True
+                        print(filename_attribute)
+                        df = pd.read_csv(filename_attribute, header=True, names=["timestamp", attribute])
+                        df.index = pd.to_datetime(df["timestamp"], unit='ms')
+                        df = df.drop("timestamp", 1)
+                        dfs.append(df)
+                    else:
+                        exists = False
+                if exists:
+                    total = pd.concat(dfs, axis=1)
+                    total = total.tz_localize('UTC').tz_convert('Asia/Kolkata')
+                    total.rename(columns=lambda x: column_mapping[x], inplace=True)
+                    total.columns.set_names(LEVEL_NAMES, inplace=True)
+                    assert total.index.is_unique
+                    store.put(str(key), total)
+    convert_yaml_to_hdf5(join(_get_module_directory(), 'metadata'),
+                         output_filename)
+
+    print("Done converting COMBED to HDF5!")
+
+
+def _get_module_directory():
+    # Taken from http://stackoverflow.com/a/6098238/732596
+    path_to_this_file = dirname(getfile(currentframe()))
+    if not isdir(path_to_this_file):
+        encoding = getfilesystemencoding()
+        path_to_this_file = dirname(unicode(__file__, encoding))
+    if not isdir(path_to_this_file):
+        abspath(getsourcefile(lambda _: None))
+    if not isdir(path_to_this_file):
+        path_to_this_file = getcwd()
+    assert isdir(path_to_this_file), path_to_this_file + ' is not a directory'
+    return path_to_this_file
---
+++ NILMTK/nilmtk/dataset_converters/eco/convert_eco.py
@@ -0,0 +1,166 @@
+import pandas as pd
+import numpy as np
+import sys
+from os import listdir, getcwd
+from os.path import isdir, join, dirname, abspath
+from pandas.tools.merge import concat
+from nilmtk.utils import get_module_directory, check_directory_exists
+from nilmtk.datastore import Key
+from nilmtk.measurement import LEVEL_NAMES
+from nilm_metadata import convert_yaml_to_hdf5
+from inspect import currentframe, getfile, getsourcefile
+from sys import getfilesystemencoding
+
+
+"""
+DATASET STRUCTURE:
+------------------
+On extracting all the dataset values, we should arrive at a similar directory structure as
+mentioned.
+
+ECO Dataset will have a folder '<i>_sm_csv' and '<i>_plug_csv' where i is the building no.
+
+<i>_sm_csv has a folder 01
+<i>_plug_csv has a folder 01, 02,....<n> where n is the plug numbers.
+
+Each folder has a CSV file as per each day, with each day csv file containing
+	86400 entries.
+"""
+
+plugs_column_name = {1: ('power', 'active')}
+
+def convert_eco(dataset_loc, hdf_filename, timezone):
+    """
+    Parameters:
+    -----------
+    dataset_loc: str
+        The root directory where the dataset is located.
+    hdf_filename: str
+        The location where the hdf_filename is present. 
+        The directory location has to contain the 
+        hdf5file name for the converter to work.
+    timezone: str
+        specifies the timezone of the dataset.
+    """
+
+    # Creating a new HDF File
+    store = pd.HDFStore(hdf_filename, 'w', complevel=9, complib='blosc')    
+    
+    check_directory_exists(dataset_loc)
+    directory_list = [i for i in listdir(dataset_loc) if '.txt' not in i]
+    directory_list.sort()
+    print(directory_list)
+
+    # Traversing every folder
+    for folder in directory_list:
+
+        if folder[0] == '.' or folder[-3:] == '.h5':
+            print('Skipping ', folder)
+            continue
+        print('Computing for folder', folder)
+
+        #Building number and meter_flag
+        building_no = int(folder[:2])
+        meter_flag = 'sm' if 'sm_csv' in folder else 'plugs'
+
+        dir_list = [i for i in listdir(join(dataset_loc, folder)) if isdir(join(dataset_loc,folder,i))]
+        dir_list.sort()
+        print('Current dir list:', dir_list)
+
+        for fl in dir_list:
+            
+            print('Computing for folder ', fl)
+            
+            fl_dir_list = [i for i in listdir(join(dataset_loc,folder,fl)) if '.csv' in i]
+            fl_dir_list.sort()
+
+            if meter_flag == 'sm':
+                for fi in fl_dir_list:
+                    df = pd.read_csv(join(dataset_loc,folder,fl,fi), names=[i for i in range(1,17)], dtype=np.float32)
+                    
+                    for phase in range(1,4):
+                        key = str(Key(building=building_no, meter=phase))
+                        df_phase = df.ix[:,[1+phase, 5+phase, 8+phase, 13+phase]]
+
+                        # get reactive power
+                        power = df_phase.as_matrix([1+phase, 13+phase])
+                        reactive = power[:,0] * np.tan(power[:,1] * np.pi / 180)
+                        df_phase['Q'] = reactive
+                        
+                        df_phase.index = pd.DatetimeIndex(start=fi[:-4], freq='s', periods=86400, tz='GMT')
+                        df_phase = df_phase.tz_convert(timezone)
+                        
+                        sm_column_name = {1+phase:('power', 'active'),
+                                            5+phase:('current', ''),
+                                            8+phase:('voltage', ''),
+                                            13+phase:('phase_angle', ''),
+                                            'Q': ('power', 'reactive'),
+                                            };
+                        df_phase.rename(columns=sm_column_name, inplace=True)
+                        
+                        tmp_before = np.size(df_phase.power.active)
+                        df_phase = df_phase[df_phase.power.active != -1]
+                        tmp_after = np.size(df_phase.power.active)
+                        if (tmp_before != tmp_after):
+                            print('Removed missing measurements - Size before: ' + str(tmp_before) + ', size after: ' + str(tmp_after))
+                        
+                        df_phase.columns.set_names(LEVEL_NAMES, inplace=True)
+                        if not key in store:
+                            store.put(key, df_phase, format='Table')
+                        else:
+                            store.append(key, df_phase, format='Table')
+                            store.flush()
+                        print('Building', building_no, ', Meter no.', phase,
+                              '=> Done for ', fi[:-4])
+                
+            else:
+                #Meter number to be used in key
+                meter_num = int(fl) + 3
+                
+                key = str(Key(building=building_no, meter=meter_num))
+                
+                #Getting dataframe for each csv file seperately
+                for fi in fl_dir_list:
+                    df = pd.read_csv(join(dataset_loc,folder,fl ,fi), names=[1], dtype=np.float64)
+                    df.index = pd.DatetimeIndex(start=fi[:-4], freq='s', periods=86400, tz = 'GMT')
+                    df.rename(columns=plugs_column_name, inplace=True)
+                    df = df.tz_convert(timezone)
+                    df.columns.set_names(LEVEL_NAMES, inplace=True)
+
+                    tmp_before = np.size(df.power.active)
+                    df = df[df.power.active != -1]
+                    tmp_after = np.size(df.power.active)
+                    if (tmp_before != tmp_after):
+                        print('Removed missing measurements - Size before: ' + str(tmp_before) + ', size after: ' + str(tmp_after))
+                    
+                    # If table not present in hdf5, create or else append to existing data
+                    if not key in store:
+                        store.put(key, df, format='Table')
+                        print('Building',building_no,', Meter no.',meter_num,'=> Done for ',fi[:-4])
+                    else:
+                        store.append(key, df, format='Table')
+                        store.flush()
+                        print('Building',building_no,', Meter no.',meter_num,'=> Done for ',fi[:-4])
+            
+    print("Data storage completed.")
+    store.close()
+
+    # Adding the metadata to the HDF5file
+    print("Proceeding to Metadata conversion...")
+    meta_path = join(_get_module_directory(), 'metadata')
+    convert_yaml_to_hdf5(meta_path, hdf_filename)
+    print("Completed Metadata conversion.")
+
+def _get_module_directory():
+    # Taken from http://stackoverflow.com/a/6098238/732596
+    path_to_this_file = dirname(getfile(currentframe()))
+    if not isdir(path_to_this_file):
+        encoding = getfilesystemencoding()
+        path_to_this_file = dirname(unicode(__file__, encoding))
+    if not isdir(path_to_this_file):
+        abspath(getsourcefile(lambda _: None))
+    if not isdir(path_to_this_file):
+        path_to_this_file = getcwd()
+    assert isdir(path_to_this_file), path_to_this_file + ' is not a directory'
+    return path_to_this_file
+
